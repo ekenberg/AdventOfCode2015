12f18674243ee6e8dd8568761f2e3b35a44acd36
Johan Ekenberg <johan.ekenberg@gmail.com>
2022-01-13 16:14:54 +0100

    Day 1 task 2

--- 1/task.hs
+++ 1/task.hs
@@ -2,3 +2,5 @@ main :: IO ()
 main = do
   input <- readFile "input.txt"
   print $ foldl (\n c->if c == '(' then n+1 else n-1) 0 input
+  print $ fst . head $ filter ((==(-1)) . snd) $
+    scanl (\(p,n) c -> if c == '(' then (p+1, n+1) else (p+1, n-1)) (0,0) input
